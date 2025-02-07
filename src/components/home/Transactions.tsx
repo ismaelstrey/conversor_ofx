1ca2a594e95a3bbef48f51e60a1a034249f8eac6
ismael strey pereira <ismaelstrey@hotmail.com>
2025-02-07 15:14:01 -0300

    🛠️ src/components/home/Transactions.tsx -> Added imports for motion effects and icon. 🛠️ src/components/home/Transactions.tsx -> Dynamically styled total display based on type. 🛠️ src/components/home/Transactions.tsx -> Clickable header to toggle transaction type. 🛠️ src/components/home/Transactions.tsx -> Conditional styling for tbody based on type.

--- src/components/home/Transactions.tsx
+++ src/components/home/Transactions.tsx
@@ -2,6 +2,8 @@ import { FIType, TransactionType } from "@/app/types/TransactionType";
 import React, { useEffect } from "react";
 import Transaction from "./Transaction";
 import { formatarParaReal } from "@/helper/FormataReal";
+import { tr } from "motion/react-client";
+import { ChevronDownIcon } from "lucide-react";
 
 export default function Transactions({
   transactions,
@@ -28,12 +30,16 @@ export default function Transactions({
           <div className="flex"> <span className="flex flex-col text-blue-500 px-2">ID:{" "}</span> {FI?.FID}</div>
           <div className="flex"><span className="flex flex-col text-blue-500 px-2">ORG:{" "}</span> {FI?.ORG}</div>
         </div>
-        <div className="flex justify-end text-5xl mr-10">Total: {totalValor}</div>
+        <div className={`flex text-3xl ${trntype === "CREDIT" ? "text-green-500" : "text-red-500"}`}>{trntype}</div>
+        <div className={`flex justify-end text-5xl mr-10 ${trntype === "CREDIT" ? "text-green-500" : "text-red-500"}`}>Total: {totalValor}</div>
       </div>
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr className="text-2xl ">
-            <th scope="col" className="px-6 py-3">TIPO</th>
+            <th scope="col" className={`flex px-6 py-3 cursor-pointer  rounded-lg ${trntype === "CREDIT" ? "text-green-500 bg-green-200" : "text-red-500 bg-red-300"}`} onClick={() => trntype === "CREDIT" ? setTrntype("DEBIT") : setTrntype("CREDIT")}>
+
+              <ChevronDownIcon />
+              TIPO</th>
             <th scope="col" className="px-6 py-3">DATA</th>
             <th scope="col" className="px-6 py-3">VALOR</th>
             <th scope="col" className="px-6 py-3">TRNAMT</th>
@@ -41,7 +47,7 @@ export default function Transactions({
             <th scope="col" className="px-6 py-3">MEMORANDO</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className={`${trntype === "CREDIT" ? "text-green-500" : "text-red-500"}`}>
           {filtrado?.map((transaction: TransactionType) => (
             <Transaction key={transaction.FITID} transaction={transaction} />
           ))}
