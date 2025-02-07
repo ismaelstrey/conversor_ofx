809f44938b3a8e5d97f8c52f9f019d31e29937c5
ismael strey pereira <ismaelstrey@hotmail.com>
2025-02-07 15:42:43 -0300

    🛠️ src/components/home/Transactions.tsx -> Updated text color styles 🛠️ src/components/home/Transactions.tsx -> Adjusted border radius for div 🛠️ src/components/home/Transactions.tsx -> Modified header Class for table 🛠️ src/components/home/Transactions.tsx -> Changed table body text color

--- src/components/home/Transactions.tsx
+++ src/components/home/Transactions.tsx
@@ -24,18 +24,18 @@ export default function Transactions({
   }, [total])
   return (
     <div className="flex flex-col container">
-      <div className="flex justify-between bg-gray-900 p-4 rounded-sm text-gray-200">
+      <div className="flex justify-between bg-gray-900 p-4 rounded-t-lg text-gray-200">
         <div className="flex flex-col ">
           <div className="flex"> <span className="flex flex-col text-blue-500 px-2">ID:{" "}</span> {FI?.FID}</div>
           <div className="flex"><span className="flex flex-col text-blue-500 px-2">ORG:{" "}</span> {FI?.ORG}</div>
         </div>
-        <div className={`flex text-3xl ${trntype === "CREDIT" ? "text-green-500" : "text-red-500"}`}>{trntype}</div>
-        <div className={`flex justify-end text-5xl mr-10 ${trntype === "CREDIT" ? "text-green-500" : "text-red-500"}`}>Total: {totalValor}</div>
+        <div className={`flex text-3xl ${trntype === "CREDIT" ? "text-green-700" : "text-red-400"}`}>{trntype}</div>
+        <div className={`flex justify-end text-5xl mr-10 ${trntype === "CREDIT" ? "text-green-700" : "text-red-400"}`}>Total: {totalValor}</div>
       </div>
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr className="text-2xl ">
-            <th scope="col" className={`flex px-6 py-3 cursor-pointer  rounded-lg ${trntype === "CREDIT" ? "text-green-500 bg-green-200" : "text-red-500 bg-red-300"}`} onClick={() => trntype === "CREDIT" ? setTrntype("DEBIT") : setTrntype("CREDIT")}>
+            <th scope="col" className={`flex px-2 m-2 py-2 cursor-pointer rounded-lg ${trntype === "CREDIT" ? "text-green-700 bg-green-200" : "text-red-700 bg-red-300"}`} onClick={() => trntype === "CREDIT" ? setTrntype("DEBIT") : setTrntype("CREDIT")}>
 
               <ChevronDownIcon />
               TIPO</th>
@@ -46,7 +46,7 @@ export default function Transactions({
             <th scope="col" className="px-6 py-3">MEMORANDO</th>
           </tr>
         </thead>
-        <tbody className={`${trntype === "CREDIT" ? "text-green-500" : "text-red-500"}`}>
+        <tbody className={`${trntype === "CREDIT" ? "text-green-800" : "text-red-900"}`}>
           {filtrado?.map((transaction: TransactionType) => (
             <Transaction key={transaction.FITID} transaction={transaction} />
           ))}
