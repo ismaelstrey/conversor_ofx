c11190e9343bae732bc1bd369ac93dbf8b2bbb40
ismael strey pereira <ismaelstrey@hotmail.com>
2025-05-05 15:24:17 -0300

    refactor(template): remove Sidebar component import and usage
    The Sidebar component was commented out to temporarily disable its functionality, likely for testing or UI adjustments. This change ensures the template layout remains consistent without the Sidebar.

--- src/components/template/index.tsx
+++ src/components/template/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Sidebar from '../sidebar'
+// import Sidebar from '../sidebar'
 
 export default function Template
     ({ children }: { children: React.ReactNode }) {
     return (
         <div className='flex bg-slate-700 min-h-screen'>
 
-            <Sidebar />
+            {/* <Sidebar /> */}
             <div className='flex-1'>
                 {children}</div>
         </div>
